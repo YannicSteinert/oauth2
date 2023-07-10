64718426bf658ad5f1c1b6c8ca52bc24effebbed
YannicSteinert <yannic.steinert@yahoo.de>
2023-07-10 09:26:50 +0200

    general cleanup
    - added url encoding option
    - implemented getters
    - reordered some code
    - fixed some names

--- lib/src/oauth2.dart
+++ lib/src/oauth2.dart
@@ -7,23 +7,35 @@ import 'dart:convert';
 
 import './token.dart';
 
+
 class OAuth2 {
   Token? token;
   final GlobalKey<NavigatorState> navigatorKey;
   String clientID, clientSecret, redirectURL, baseURL;
   String authorizationEndpoint, tokenEndpoint;
   String codeVerifier = '', scope = '', state = '';
+  bool urlEncoded;
 
-  OAuth2(
-      {required this.authorizationEndpoint,
-      required this.tokenEndpoint,
-      required this.clientID,
-      required this.clientSecret,
-      required this.redirectURL,
-      required this.navigatorKey,
-      this.scope = '',
-      this.baseURL = '',
-      });
+  OAuth2({
+    required this.authorizationEndpoint,
+    required this.tokenEndpoint,
+    required this.clientID,
+    required this.clientSecret,
+    required this.redirectURL,
+    required this.navigatorKey,
+    this.scope = '',
+    this.baseURL = '',
+    this.urlEncoded = true,
+  });
+
+  /// Getters
+  bool get isAuthed {
+    loadFromStorage();
+    if (token!.isValid()) {
+      return true;
+    }
+    return false;
+  }
 
   Map<String, String> get authHeaders {
     loadFromStorage();
@@ -32,24 +44,125 @@ class OAuth2 {
     };
   }
 
-  String _generateLoginUrl() {
+  String get authorizationURL {
     return '$baseURL$authorizationEndpoint';
   }
 
-  String _generateTokenUrl() {
+  String get tokenURL {
     return '$baseURL$tokenEndpoint';
   }
 
+  /// Methods
+  String LoginRedirectUrl({String? scope_ = null}) {
+    state = _encodeState();
+
+    codeVerifier = _generateCodeVerifier();
+    html.window.sessionStorage['FLUTTER_APP_$state'] = codeVerifier;
+
+    String challenge = _generateCodeChallenge(codeVerifier);
+    String url = '$authorizationURL?'
+        'response_type=code'
+        '&prompt=consent&access_type=offline' // @todo: to force refresh token
+        '&code_challenge_method=S256'
+        '&code_challenge=$challenge'
+        '&client_id=$clientID'
+        '&redirect_uri=$redirectURL'
+        '&state=$state';
+
+    _generateScope(scope_);
+    if (scope.isNotEmpty) {
+      url += '&scope=$scope';
+    }
+    return url;
+  }
+
+  Future<void> LoginCallback(RouteSettings settings) async {
+    if (settings.name == null) return;
+
+    Map queryParameters;
+    var uriData = Uri.parse(settings.name!);
+    queryParameters = uriData.queryParameters;
+
+    if (!queryParameters.containsKey('code')) return;
+    if (queryParameters.containsKey('state')) state = queryParameters['state'];
+
+    /// flutter bug workaround
+    /// https://github.com/flutter/flutter/issues/71786
+    if (queryParameters['code'].length < 2) return;
+    if (html.window.sessionStorage.containsKey('__FLUTTER_GENERATE_ROUTE__'))
+      return;
+    html.window.sessionStorage['__FLUTTER_GENERATE_ROUTE__'] = '#';
+
+    _handleTokenResponseState();
+
+    String url = tokenURL;
+    var body = urlEncoded
+        ? _generateTokenRequestBodyUrlEncoded(queryParameters['code'])
+        : _generateTokenRequestBodyJson(queryParameters['code']);
+    Uri uri = Uri.parse(url);
+    http.Response response = await http
+        .post(uri, body: urlEncoded ? body : jsonEncode(body), headers: {
+      'Content-type':
+          urlEncoded ? 'application/x-www-form-urlencoded' : 'application/json',
+    });
+
+    Map<String, dynamic> resBody = jsonDecode(response.body);
+    _handleTokenResponseCode(response.statusCode, resBody);
+
+    html.window.history.pushState({}, '', '/callback');
+
+    /// Clean up flutter bug workaround
+    html.window.sessionStorage.remove('__FLUTTER_GENERATE_ROUTE__');
+  }
+
+  void loadFromStorage() {
+    token = Token.fromSession();
+    if (token!.hasRefresh() && token!.isValid()) return;
+    token = Token.fromStorage();
+  }
+
+  void silentLogin() {
+    token = Token.fromSession();
+    if (token!.isValid()) return;
+    if (token!.hasRefresh()) {
+      RefreshToken();
+    }
+
+    token = Token.fromStorage();
+    if (token == null) return;
+    if (token!.isValid()) return;
+
+    if (token!.hasRefresh()) {
+      RefreshToken();
+    }
+  }
+
+  void RefreshToken() async {
+    var body = urlEncoded
+        ? _generateRefreshRequestBodyUrlEncoded
+        : _generateRefreshRequestBodyJson();
+    Uri uri = Uri.parse(tokenURL);
+    http.Response response = await http
+        .post(uri, body: urlEncoded ? body : jsonEncode(body), headers: {
+      'Content-type':
+          urlEncoded ? 'application/x-www-form-urlencoded' : 'application/json',
+    });
+
+    Map<String, dynamic> resBody = jsonDecode(response.body);
+    _handleTokenRefreshCode(response.statusCode, resBody);
+  }
+
+  /// Private Methods
   String _generateCodeVerifier({int length = 64}) {
     var rng = Random();
     const String charset =
         '2Pb3fHSdoziWjtFVkn9JUhlpIEgTxMKAv51qDrXR4LOe7CBYy0auZQmw8sGc6N';
 
-    String challenge = '';
+    String verifier = '';
     for (int i = 0; i < length; i++) {
-      challenge += charset[rng.nextInt(charset.length)];
+      verifier += charset[rng.nextInt(charset.length)];
     }
-    return challenge;
+    return verifier;
   }
 
   String _generateCodeChallenge(String verifier) {
@@ -63,19 +176,6 @@ class OAuth2 {
   }
 
   void _decodeState(String _state) {}
-
-  String _generateRedirectURL() {
-    String challenge = _generateCodeChallenge(codeVerifier);
-
-    return '${_generateLoginUrl()}?'
-        'response_type=code'
-        '&prompt=consent&access_type=offline' // @todo: to force refresh token
-        '&code_challenge_method=S256'
-        '&code_challenge=$challenge'
-        '&client_id=$clientID'
-        '&redirect_uri=$redirectURL'
-        '&state=$state';
-  }
 
   void _generateScope(String? _scope) {
     if (html.window.localStorage.containsKey('FLUTTER_APP_SCOPE')) {
@@ -98,22 +198,7 @@ class OAuth2 {
     return base64Encode(_state.codeUnits);
   }
 
-  String LoginRedirectUrl({String? scope_ = null}) {
-    state = _encodeState();
-
-    codeVerifier = _generateCodeVerifier();
-    html.window.sessionStorage['FLUTTER_APP_$state'] = codeVerifier;
-
-    _generateScope(scope_);
-    String url = _generateRedirectURL();
-
-    if (scope.isNotEmpty) {
-      url += '&scope=$scope';
-    }
-    return url;
-  }
-
-  Map<String, String> _generateTokenRequestBody(String code) {
+  Map<String, String> _generateTokenRequestBodyJson(String code) {
     return {
       'grant_type': 'authorization_code',
       'code': code,
@@ -122,6 +207,15 @@ class OAuth2 {
       'client_id': '$clientID',
       'client_secret': '$clientSecret'
     };
+  }
+
+  String _generateTokenRequestBodyUrlEncoded(String code) {
+    return 'grant_type=authorization_code'
+        '&code=$code'
+        '&redirect_uri=$redirectURL'
+        '&code_verifier=$codeVerifier'
+        '&client_id=$clientID'
+        '&client_secret=$clientSecret';
   }
 
   void _handleTokenResponseCode(int status, Map<String, dynamic> responseBody) {
@@ -164,75 +258,6 @@ class OAuth2 {
     }
   }
 
-  Future<void> LoginCallback(RouteSettings settings) async {
-    if (settings.name == null) return;
-
-    Map queryParameters;
-    var uriData = Uri.parse(settings.name!);
-    queryParameters = uriData.queryParameters;
-
-    if (!queryParameters.containsKey('code')) return;
-    if (queryParameters.containsKey('state')) state = queryParameters['state'];
-
-    /// flutter bug workaround
-    /// https://github.com/flutter/flutter/issues/71786
-    if (queryParameters['code'].length < 2) return;
-    if (html.window.sessionStorage.containsKey('__FLUTTER_GENERATE_ROUTE__'))
-      return;
-    html.window.sessionStorage['__FLUTTER_GENERATE_ROUTE__'] = '#';
-
-    _handleTokenResponseState();
-
-    String url = _generateTokenUrl();
-    var body = _generateTokenRequestBody(queryParameters['code']);
-    Uri uri = Uri.parse(url);
-    http.Response response = await http.post(uri,
-        body: jsonEncode(body),
-        headers: {
-          'Content-type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        });
-
-    Map<String, dynamic> resBody = jsonDecode(response.body);
-    _handleTokenResponseCode(response.statusCode, resBody);
-
-    html.window.history.pushState({}, '', '/callback');
-
-    /// Clean up flutter bug workaround
-    html.window.sessionStorage.remove('__FLUTTER_GENERATE_ROUTE__');
-  }
-
-
-  void loadFromStorage() {
-    token = Token.fromSession();
-    if(token!.hasRefresh() && token!.isValid()) return;
-    token = Token.fromStorage();
-  }
-
-  void silentLogin() {
-    token = Token.fromSession();
-    if (token!.isValid()) return;
-    if (token!.hasRefresh()) {
-      RefreshToken();
-    }
-
-    token = Token.fromStorage();
-    if (token == null) return;
-    if (token!.isValid()) return;
-
-    if (token!.hasRefresh()) {
-      RefreshToken();
-    }
-  }
-
-  bool get isAuthed {
-    loadFromStorage();
-    if (token!.isValid()) {
-      return true;
-    }
-    return false;
-  }
-
   void _handleTokenRefreshCode(int status, Map<String, dynamic> responseBody) {
     switch (status) {
       case 200: // OK
@@ -250,7 +275,7 @@ class OAuth2 {
         token!.StoreInStorage();
         break;
 
-    /// @todo: Error Handling
+      /// @todo: Error Handling
       case 400: // Bad Request
       case 401: // Unauthorized
       case 403: // Forbidden
@@ -267,7 +292,7 @@ class OAuth2 {
     }
   }
 
-  Map<String, String> _generateRefreshRequestBody() {
+  Map<String, String> _generateRefreshRequestBodyJson() {
     return {
       'grant_type': 'refresh_token',
       'refresh_token': '${token!.refresh}',
@@ -276,17 +301,10 @@ class OAuth2 {
     };
   }
 
-  void RefreshToken() async {
-    var body = _generateRefreshRequestBody();
-    Uri uri = Uri.parse(_generateTokenUrl());
-    http.Response response = await http.post(uri,
-        body: jsonEncode(body),
-        headers: {
-          'Content-type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        });
-
-    Map<String, dynamic> resBody = jsonDecode(response.body);
-    _handleTokenRefreshCode(response.statusCode, resBody);
+  String _generateRefreshRequestBodyUrlEncoded() {
+    return 'grant_type=refresh_token'
+        '&refresh_token=${token!.refresh}'
+        '&client_id=$clientID'
+        '&client_secret=$clientSecret';
   }
 }
