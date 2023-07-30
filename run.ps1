ebacb40972ef359f9057881298ab0450b3f27e7a
YannicSteinert <74675755+YannicSteinert@users.noreply.github.com>
2023-07-30 12:02:52 +0200

    Change dhttpd port

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
 flutter clean
 flutter pub get
 flutter build web --release --web-renderer html
-dhttpd -p 4000 --path=./build/web --host=127.0.0.1
+dhttpd -p 8080 --path=./build/web --host=127.0.0.1
